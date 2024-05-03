84270155b34a43b5a87cee866a69e3f50e5f4cf8
Donald Keita <keitadonald>
2024-05-03 18:53:45 -0400

    initial commit

---
+++ backend/src/main/java/com/revature/nile/models/Order.java
@@ -0,0 +1,104 @@
+package com.revature.nile.models;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.data.jpa.repository.Temporal;
+
+@Entity
+@Table(name = "orders")
+public class Order {
+
+        public enum StatusEnum {
+            PENDING,
+            APPROVED,
+            COMPLETED
+        }
+
+        // Data Fields
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private int orderId;
+
+        // Foreign
+        @ManyToOne
+        @JoinColumn(nullable = false, name = "user_id")
+        @JsonBackReference
+        private User user;
+
+        private StatusEnum status;
+
+        private String shipToAddress;
+        private String billAddress;
+
+        @Temporal(TemporalType.TIMESTAMP)
+        @CreationTimestamp
+        private String dateOrdered;
+
+    // Constructors
+
+    // no-args constructor
+    public Order() {}
+    // all-args constructor
+    public Order(int orderId, User user, StatusEnum status, String shipToAddress, String billAddress, String dateOrdered) {
+        this.orderId = orderId;
+        this.user = user;
+        this.status = status;
+        this.shipToAddress = shipToAddress;
+        this.billAddress = billAddress;
+        this.dateOrdered = dateOrdered;
+    }
+
+
+    // Getters and Setters
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public StatusEnum getStatus() {
+        return status;
+    }
+
+    public void setStatus(StatusEnum status) {
+        this.status = status;
+    }
+
+    public String getShipToAddress() {
+        return shipToAddress;
+    }
+
+    public void setShipToAddress(String shipToAddress) {
+        this.shipToAddress = shipToAddress;
+    }
+
+    public String getBillAddress() {
+        return billAddress;
+    }
+
+    public void setBillAddress(String billAddress) {
+        this.billAddress = billAddress;
+    }
+
+    public String getDateOrdered() {
+        return dateOrdered;
+    }
+
+    public void setDateOrdered(String dateOrdered) {
+        this.dateOrdered = dateOrdered;
+    }
+
+
+}
