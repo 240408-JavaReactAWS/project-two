9c6f4694c515a9460bd45b678b5acdc17a826137
pekellogg <peyton846@revature.net>
2024-05-03 07:55:22 -0700

    initial commit

---
+++ backend/src/main/java/com/revature/nile/NileApplication.java
@@ -0,0 +1,13 @@
+package com.revature.nile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NileApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/revature/nile/NileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.nile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
