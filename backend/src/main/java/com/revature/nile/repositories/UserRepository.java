b8db67d00d2e20e12001653396e49629c06e947d
EliHedrick <eli.hedrickk@gmail.com>
2024-05-03 15:59:12 -0400

    User Service and new User repo method

--- backend/src/main/java/com/revature/nile/repositories/UserRepository.java
+++ backend/src/main/java/com/revature/nile/repositories/UserRepository.java
@@ -10,5 +10,5 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Integer> {
 
     Optional<User> findByUsername(String username);
-
+    Optional<User> findByEmail(String username);
 }
