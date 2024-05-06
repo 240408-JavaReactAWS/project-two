69bc11248651431179d9f3a5b8b011811fa25aa4
lggarcia0 <luisgarcia@alumni.unc.edu>
2024-05-06 14:58:51 -0400

    added boiler plate code to nav

---
+++ frontend/src/components/common/Nav.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
+
+const Nav: React.FC = () => {
+
+  return (
+    <div>Nav</div>
+  )
+}
+
+export default Nav;
