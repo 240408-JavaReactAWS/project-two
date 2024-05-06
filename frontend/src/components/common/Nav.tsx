8197d7569e0851cd902c0b3673a0dba5fd8ee86f
PhDiction <tyler.e.morton@gmail.com>
2024-05-06 15:49:49 -0400

    Added conditional rendering

--- frontend/src/components/common/Nav.tsx
+++ frontend/src/components/common/Nav.tsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
 
 const Nav: React.FC = () => {
+
+  const [loggedIn, setLoggedIn] = useState<boolean>(true)
 
   return (
     <>
@@ -16,9 +18,17 @@ const Nav: React.FC = () => {
             <li className="nav-item">
               <Link to="/products" className="nav-link">Browse Products</Link>
             </li>
+          {!loggedIn &&
             <li className="nav-item">
               <Link to="/login" className="nav-link">Login</Link>
             </li>
+          }
+            {loggedIn && <><li className="nav-item"><Link to="#" className="nav-link">My Listings</Link></li>
+            <li className="nav-item"><Link to="#" className="nav-link">My Orders</Link></li>
+            <li className="nav-item"><Link to="#" className="nav-link">Cart</Link></li>
+            <li className="nav-item"><Link to="#" className="nav-link">Checkout</Link></li>
+            <li className="nav-item"><Link to="#" className="nav-link">Logout</Link></li>
+            </>}
           </ul>
         </div>
       </div>
