f14ee0e2d3b1375ded653abd4f4a51fb7a58b866
lggarcia0 <luisgarcia@alumni.unc.edu>
2024-05-06 17:46:14 -0400

    Changed the paths on Nav to reflect main App.tsx

--- frontend/src/components/common/Nav.tsx
+++ frontend/src/components/common/Nav.tsx
@@ -1,14 +1,14 @@
 import React, { useContext, useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
 import 'bootstrap/dist/js/bootstrap.bundle.min'; // Import Bootstrap JS
 import "./Nav.css"; // Import custom CSS
 import axios from 'axios';
 import { UserContext } from '../../App';
-
 const Nav: React.FC = () => {
 
   const {username, setUsername} = useContext<any>(UserContext)
+  const navigateTo = useNavigate();
 
   let logout = async() => {
     try {
@@ -18,6 +18,7 @@ const Nav: React.FC = () => {
       if (res.status === 200) {
           console.log("Logout Successful")
           setUsername('')
+          navigateTo('/')
       }
     } catch (error) {
       alert("There was an error logging out")
@@ -38,17 +39,17 @@ const Nav: React.FC = () => {
               <Link to="/" className="nav-link">Nile</Link>  
             </li>
             <li className="nav-item p-2">
-              <Link to="/products" className="nav-link">Browse Products</Link>
+              <Link to="/" className="nav-link">Browse Products</Link>
             </li>
           {!username &&
             <li className="nav-item p-2">
               <Link to="/login" className="nav-link">Login</Link>
             </li>
           }
-            {username && <><li className="nav-item p-2"><Link to="#" className="nav-link">My Listings</Link></li>
-            <li className="nav-item p-2"><Link to="#" className="nav-link">My Orders</Link></li>
-            <li className="nav-item p-2"><Link to="#" className="nav-link">Cart</Link></li>
-            <li className="nav-item p-2"><Link to="#" className="nav-link">Checkout</Link></li>
+            {username && <><li className="nav-item p-2"><Link to="/listings" className="nav-link">My Listings</Link></li>
+            <li className="nav-item p-2"><Link to="/orders" className="nav-link">My Orders</Link></li>
+            <li className="nav-item p-2"><Link to="/cart" className="nav-link">Cart</Link></li>
+            <li className="nav-item p-2"><Link to="/checkout" className="nav-link">Checkout</Link></li>
             <li className="nav-item p-2"><button onClick={logout} className="nav-link">Logout</button></li>
             </>}
           </ul>
