d225d7cd9e8453f12935ceb6c3266e96a4466b86
pekellogg <peyton846@revature.net>
2024-05-06 20:45:36 -0700

    specifies imports to reduce imported libraries

--- backend/src/main/java/com/revature/nile/controllers/UserController.java
+++ backend/src/main/java/com/revature/nile/controllers/UserController.java
@@ -4,10 +4,14 @@ import com.revature.nile.models.User;
 import com.revature.nile.services.UserService;
 import jakarta.persistence.EntityExistsException;
 import jakarta.persistence.EntityNotFoundException;
+import javax.naming.AuthenticationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import javax.naming.AuthenticationException;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import static org.springframework.http.HttpStatus.*;
 
 @RestController
