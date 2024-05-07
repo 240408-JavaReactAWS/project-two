f2951350448acd1d0b0e4ca8ab7b777ec7836819
PhDiction <tyler.e.morton@gmail.com>
2024-05-07 14:45:23 -0400

    Added boilerplate for browsing page

---
+++ frontend/src/components/pages/BrowsingPage.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
+import 'bootstrap/dist/js/bootstrap.bundle.min'; // Import Bootstrap JS
+import ItemCard from '../common/ItemCard';
+
+function BrowsingPage() {
+
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default BrowsingPage
