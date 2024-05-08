7dee0da525aafb163a1b54d6e4bec63ff9456f1e
EliHedrick <eli.hedrickk@gmail.com>
2024-05-08 14:44:10 -0400

    added function to create items in database. Body of request is item and request header has  userId. Throws error if user doesn;t exist or if item fields are bad

--- backend/src/main/java/com/revature/nile/controllers/ItemController.java
+++ backend/src/main/java/com/revature/nile/controllers/ItemController.java
@@ -1,7 +1,9 @@
 package com.revature.nile.controllers;
 
+import com.revature.nile.exceptions.ItemNotCreatedException;
 import com.revature.nile.models.Item;
 import com.revature.nile.models.Review;
+import com.revature.nile.models.User;
 import com.revature.nile.services.ItemService;
 import com.revature.nile.services.ReviewService;
 import com.revature.nile.services.UserService;
@@ -9,16 +11,12 @@ import com.revature.nile.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import static org.springframework.http.HttpStatus.*;
 
 import java.util.List;
+import java.util.Optional;
 
 import jakarta.persistence.EntityNotFoundException;
 
@@ -41,10 +39,16 @@ public class ItemController {
      * In actual implementation, the sellerId should be retrieved from the header.
      */
     @PostMapping
-    public ResponseEntity<Item> createItem(@RequestBody Item item, @RequestParam int sellerId) {
-        Item newItem = item;
-        newItem.setUser(userService.getUserById(sellerId));
-        return new ResponseEntity<>(itemService.createItem(newItem), CREATED);
+    public ResponseEntity<Item> addNewItemHandler(@RequestBody Item item, @RequestHeader(name="userId") int userId) {
+        Item newItem;
+        try {
+            newItem = itemService.addNewItem(item, userId);
+        } catch (ItemNotCreatedException e) {
+            return new ResponseEntity<>(BAD_REQUEST);
+        } catch (EntityNotFoundException e) {
+            return new ResponseEntity<>(NOT_FOUND);
+        }
+        return new ResponseEntity<>(newItem, CREATED);
     }
 
 
