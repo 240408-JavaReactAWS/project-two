e68bdbea53bfd0818f61da94a6431446101a8c2b
Matthew Kwon <matthew.o.kwon@gmail.com>
2024-05-08 13:25:15 -0700

    Minor fix to patch request; fixed endpoints

--- frontend/src/components/common/ItemForm.tsx
+++ frontend/src/components/common/ItemForm.tsx
@@ -26,7 +26,7 @@ const ItemForm: React.FC<IItemFormProps> = ({ itemId }) => {
   // Check if item already exists
   useEffect(() => {
     if (itemId) {
-      axios.get(`${process.env.BACKEND_URL}/items/${itemId}`)
+      axios.get(`${process.env.REACT_APP_API_URL}/items/${itemId}`)
         .then(response => {
           const { name, description, price, stock, datePosted, image } = response.data;
           setName(name);
@@ -77,10 +77,9 @@ const ItemForm: React.FC<IItemFormProps> = ({ itemId }) => {
         price: parseFloat(price.replace(/,/g, '')),
         stock,
         image,
-        datePosted,
       };
       const axiosMethod = itemId ? axios.patch : axios.post;
-      const url = itemId ? `${process.env.BACKEND_URL}/items/${itemId}` : `${process.env.BACKEND_URL}/items`;
+      const url = itemId ? `${process.env.REACT_APP_API_URL}/items/${itemId}` : `${process.env.REACT_APP_API_URL}/items`;
 
       axiosMethod(url, itemData)
         .then(response => console.log(response))
