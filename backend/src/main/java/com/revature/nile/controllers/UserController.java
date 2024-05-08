9aac7a7ce7bf2c3225c3cd5b4a9475a2ea3d958a
EliHedrick <eli.hedrickk@gmail.com>
2024-05-08 15:51:13 -0400

    method to add items to cart. Invalid Auth returns 403, Bad quantity or null/ bad item returns 400, If item is already in cart it adds to quantity instead of making aduplicate in the order

--- backend/src/main/java/com/revature/nile/controllers/UserController.java
+++ backend/src/main/java/com/revature/nile/controllers/UserController.java
@@ -4,6 +4,7 @@ import com.revature.nile.models.Item;
 import com.revature.nile.models.Order;
 import com.revature.nile.models.OrderItem;
 import com.revature.nile.models.User;
+import com.revature.nile.services.ItemService;
 import com.revature.nile.services.OrderService;
 import com.revature.nile.services.UserService;
 import jakarta.persistence.EntityExistsException;
@@ -11,12 +12,8 @@ import jakarta.persistence.EntityNotFoundException;
 import javax.naming.AuthenticationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
+
 import static org.springframework.http.HttpStatus.*;
 
 import java.util.ArrayList;
@@ -27,11 +24,13 @@ import java.util.List;
 public class UserController  {
     private final UserService us;
     private final OrderService os;
+    private final ItemService is;
 
     @Autowired
-    public UserController(UserService us, OrderService os) {
+    public UserController(UserService us, OrderService os, ItemService is) {
         this.us = us;
         this.os = os;
+        this.is = is;
     }
 
     @PostMapping("register")
@@ -92,32 +91,33 @@ public class UserController  {
      * Currently, we have to have the entire Item object in the orderItem.
      */
     @PostMapping("{id}/orders/current")
-    public ResponseEntity<Order> addItemToOrderHandler(@PathVariable("id") int userId, @RequestBody OrderItem orderItem) {
+    public ResponseEntity<Order> addItemToOrderHandler(@PathVariable("id") int id, @RequestHeader(name="userId") int userId, @RequestBody OrderItem orderItem) {
+        if(id != userId)
+            return new ResponseEntity<>(FORBIDDEN);
         User user;
-        System.out.println(orderItem.toString());
         // Get the user by ID
         try {
             user = us.getUserById(userId);
         } catch (EntityNotFoundException e) {
             return new ResponseEntity<>(NOT_FOUND);
         }
-        //If the user does not have a current order, create a new order for them
-        Order currOrder = os.getCurrentOrderByUserId(userId);
-        if (currOrder == null) {
-            Order newOrder = new Order();
-            newOrder.setUser(user);
-            newOrder.setStatus(Order.StatusEnum.PENDING);
-            newOrder.setBillAddress(user.getAddress());
-            newOrder.setShipToAddress(user.getAddress());
-            newOrder.setOrderItems(new ArrayList<OrderItem>());
-            os.createOrder(newOrder);
-            currOrder = newOrder;
+        if(orderItem.getItem() == null || orderItem.getQuantity() <= 0)
+            return new ResponseEntity<>(BAD_REQUEST);
+        Item item;
+        try {
+            item = is.getItemById(orderItem.getItem().getItemId());
+        } catch (EntityNotFoundException e) {
+            return new ResponseEntity<>(BAD_REQUEST);
         }
-        orderItem.setOrder(currOrder);
-        os.createOrderItem(orderItem);
-        currOrder.getOrderItems().add(orderItem);
+        if(item.getStock() < orderItem.getQuantity())
+            return new ResponseEntity<>(BAD_REQUEST);
+        orderItem.setItem(item);
+        //If the user does not have a current order, create a new order for them
+        Order finalOrder = os.addOrderItemToCart(userId, orderItem);
+        if(finalOrder == null)
+            return new ResponseEntity<>(NOT_FOUND);
 
-        return new ResponseEntity<Order>(os.getOrderById(currOrder.getOrderId()), CREATED);
+        return new ResponseEntity<Order>(os.getOrderById(finalOrder.getOrderId()), CREATED);
     }
 
 }
--- backend/src/main/java/com/revature/nile/services/OrderService.java
+++ backend/src/main/java/com/revature/nile/services/OrderService.java
@@ -1,10 +1,12 @@
 package com.revature.nile.services;
 
+import com.revature.nile.models.Item;
 import com.revature.nile.models.Order;
 import com.revature.nile.models.OrderItem;
 import com.revature.nile.models.User;
 import com.revature.nile.repositories.OrderItemRepository;
 import com.revature.nile.repositories.OrderRepository;
+import com.revature.nile.repositories.UserRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -14,11 +16,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 @Service
 public class OrderService {
       private final OrderRepository orderRepository;
+      private final UserRepository userRepository;
       private final OrderItemRepository orderItemRepository;
 
       @Autowired
-      public OrderService(OrderRepository orderRepository, OrderItemRepository orderItemRepository) {
+      public OrderService(OrderRepository orderRepository, UserRepository userRepository, OrderItemRepository orderItemRepository) {
             this.orderRepository = orderRepository;
+            this.userRepository = userRepository;
             this.orderItemRepository = orderItemRepository;
       }
 
@@ -51,5 +55,35 @@ public class OrderService {
                   return order.get().getUser();
             }
             return null;
+      }
+
+      public Order addOrderItemToCart(int userId, OrderItem orderItem) {
+            Optional<User> optionalUser = userRepository.findById(userId);
+            if(optionalUser.isEmpty()) {
+                  return null;
+            }
+            User user = optionalUser.get();
+            Order currOrder = this.getCurrentOrderByUserId(userId);
+            if (currOrder == null) {
+                  Order newOrder = new Order();
+                  newOrder.setUser(user);
+                  newOrder.setStatus(Order.StatusEnum.PENDING);
+                  newOrder.setBillAddress(user.getAddress());
+                  newOrder.setShipToAddress(user.getAddress());
+                  newOrder.setOrderItems(new ArrayList<OrderItem>());
+                  this.createOrder(newOrder);
+                  currOrder = newOrder;
+            }
+            orderItem.setOrder(currOrder);
+            for(OrderItem cartItems : currOrder.getOrderItems()) { // if item id is already in cart, add quantity
+                  if(cartItems.getItem().getItemId() == orderItem.getItem().getItemId()) {
+                        cartItems.setQuantity(cartItems.getQuantity() + orderItem.getQuantity());
+                        orderItemRepository.save(cartItems);
+                        return currOrder;
+                  }
+            }
+            this.createOrderItem(orderItem);
+            currOrder.getOrderItems().add(orderItem);
+            return currOrder;
       }
 }
