43c88f52dc3b9eebe8e90ef9567bda20f31818e8
sir-shogun <kwt7311@comcast.net>
2024-05-09 14:37:59 -0400

    Stock checking added before purchase

--- frontend/src/components/pages/CheckoutPage/SummaryRow.tsx
+++ frontend/src/components/pages/CheckoutPage/SummaryRow.tsx
@@ -3,6 +3,7 @@ import { IOrderItem } from "../../../interfaces/IOrderItem";
 import axios from "axios";
 import { IItem } from "../../../interfaces/IItem";
 import { UserContext } from "../../../App";
+import { useNavigate } from "react-router-dom";
 
 interface IProps {
     item: IOrderItem,
@@ -14,6 +15,7 @@ function SummaryRow({item, total, setTotal}: IProps) {
 
     const { userId } = useContext(UserContext);
     const [itemData, setItemData] = useState<IItem>();
+    const navigate = useNavigate();
 
     useEffect(() => {
         axios.get(`${process.env.REACT_APP_API_URL}/items/${item.itemId}`, {
@@ -25,6 +27,10 @@ function SummaryRow({item, total, setTotal}: IProps) {
         })
         .then(response => {
             setItemData(response.data);
+            if (item.quantity > response.data.stock) {
+                alert("Sorry, we don't have that many " + response.data.name + " in stock!");
+                setTimeout(() => navigate('/CartPage'), 1000);
+            }
             setTotal(total + (response.data.price * item.quantity));
         })
         .catch(error => {
